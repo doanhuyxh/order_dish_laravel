582ebfa89c365245b50d3f8f5c418f9b6d4d0684
doanhuyxh <doanhuyxh@gmail.com>
2024-05-10 13:22:08 +0700

    init

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div class="form flex-column justify-content-center text-center">
+    <div class="form flex-column justify-content-center text-center" id="formDish">
 
         <div class="btn-group m-auto my-3" role="group">
             <button type="button" class="btn btn-outline-primary" :class="{ 'active': step == 1 }">Step 1</button>
@@ -16,10 +16,11 @@
 
         <div class="form-group w-50 m-auto" v-if="step==1">
             <div class="form-group my-2">
+
                 <label class="form-label">Please Select a meal</label>
-                <select class="form-select" v-model="dataSubmit.meal">
+                <select class="form-select" v-model="dataSubmit.meal" >
                     <option selected disabled></option>
-                    <option v-for="(item) in meals" :value="item" :key="item.id" v-text="item.name"></option>
+                    <option v-for="(item) in meals" :value="item" :key="item" v-text="item"></option>
 
                 </select>
             </div>
@@ -33,9 +34,9 @@
         <div class="form-group w-50 m-auto" v-if="step==2">
             <div class="form-group my-2">
                 <label class="form-label">Please Select a Restaurant</label>
-                <select class="form-select" v-model="dataSubmit.restaurant">
+                <select class="form-select" v-model="dataSubmit.restaurant" >
                     <option selected disabled></option>
-                    <option v-for="(item) in restaurants" :value="item" :key="item.id" v-text="item.name"></option>
+                    <option v-for="(item) in restaurants" :value="item" :key="item" v-text="item"></option>
                 </select>
             </div>
         </div>
@@ -46,7 +47,7 @@
                  :key="index">
                 <div class="form-group my-2 ">
                     <label class="form-label">Please Select a Dish</label>
-                    <select class="form-select" v-model="dataSubmit.dish[index].dish">
+                    <select class="form-select" v-model="dataSubmit.dish[index].dish" v-on:change="filteredDishesAdd">
                         <option selected></option>
                         <option v-for="(item) in dish" :value="item" :key="item.id" v-text="item.name"></option>
                     </select>
@@ -60,7 +61,7 @@
 
             </div>
             <div class="d-flex w-25 m-auto">
-                <span class="btn" v-on:click="addDish()">+</span>
+                <span class="btn btn-success" v-on:click="addDish()">+</span>
             </div>
         </div>
 
@@ -70,7 +71,7 @@
                 <div class="row p-3">
                     <div class="col-6">Meal</div>
                     <div class="col-6">
-                        <span v-text="dataSubmit.meal.name"></span>
+                        <span v-text="dataSubmit.meal"></span>
                     </div>
                 </div>
                 <div class="row p-3">
@@ -82,7 +83,7 @@
                 <div class="row p-3">
                     <div class="col-6">Restaurant</div>
                     <div class="col-6">
-                        <span v-text="dataSubmit.restaurant.name"></span>
+                        <span v-text="dataSubmit.restaurant"></span>
                     </div>
                 </div>
                 <div class="row p-3">
@@ -113,12 +114,13 @@
     <script>
 
         var app_vue = new Vue({
-            el: ".form",
+            el: "#formDish",
             data: {
                 step: 1,
-                meals: @json($meals),
-                restaurants: @json($restaurants),
-                dish:@json($dish),
+                meals: [],
+                restaurants:[] ,
+                dish:[],
+                dishMain: [],
                 dataSubmit: {
                     meal: null,
                     restaurant: null,
@@ -126,20 +128,86 @@
                     numberPerson: 1
                 }
             },
+
+
             mounted() {
                 this.initData();
             },
             methods: {
+                filteredDishesMeal() {
+                    this.dish = [...this.dishMain];
+                    if (this.dataSubmit.meal) {
+                        let filteredByRestaurant = this.dish.filter(dishes => dishes.availableMeals.includes(this.dataSubmit.meal));
+                        this.restaurants = [...new Set(filteredByRestaurant.map(dish => dish.restaurant))];
+
+                        return filteredByRestaurant;
+                    }
+                    else {
+                        return this.dish;
+                    }
+                },
+                filteredDishesMealRestaurant(){
+                    this.dish = [...this.dishMain];
+
+                    if (this.dataSubmit.restaurant) {
+                        let filteredByRestaurant = this.dish.filter(dish => dish.restaurant === this.dataSubmit.restaurant);
+                        return filteredByRestaurant;
+
+                    }
+                    else {
+
+                        return this.dish;
+                    }
+                },
+                filteredAll(){
+                    this.dish = [...this.dishMain];
+
+                    if (this.dataSubmit.meal && this.dataSubmit.restaurant) {
+                        return this.dish.filter(dishes => {
+                            return dishes.availableMeals.includes(this.dataSubmit.meal) && dishes.restaurant === this.dataSubmit.restaurant;
+                        });
+
+                    }
+                    else {
+                        return this.dish;
+                    }
+                },
+                filteredDishesAdd() {
+                    let newDishName = this.dataSubmit.dish[this.dataSubmit.dish.length - 1].dish.name;
+
+                    if (this.dataSubmit.dish.some(item => item.dish.name === newDishName && item !== this.dataSubmit.dish[this.dataSubmit.dish.length - 1])) {
+                        alert('This dish has already been selected');
+                        this.dataSubmit.dish.pop();
+                    }
+
+                },
                 initData() {
+                    fetch('/dishes.json')
+                        .then(response => response.json())
+                        .then(data => {
+                            this.dish = data.dishes;
+                            this.dishMain = data.dishes;
+                            this.meals = [...new Set(this.dish.flatMap(dish => dish.availableMeals))];
+                            console.log(this.dish);
+
+                        })
+                        .catch(error => {
+                            console.error('Error fetching data:', error);
+                        });
                 },
                 addDish() {
+
                     let obj = {
                         dish: null,
                         number: 1
                     }
-                    this.dataSubmit.dish.push(obj)
+
+                    this.dataSubmit.dish.push(obj);
+                    console.log(this.dataSubmit.dish);
+
                 },
                 submit() {
+
                     let currentThis = this;
                     let dish = []
                     currentThis.dataSubmit.dish.forEach(item=>{
@@ -174,16 +242,40 @@
                     let check = false;
                     switch (this.step) {
                         case 1:
-                            if (this.dataSubmit.numberPerson < 1) {
-                                check = true;
-                                alert("Enter people")
+                            if (!this.dataSubmit.meal) {
+                                alert('Please Select a meal');
+                                return;
                             }
+                            if (this.dataSubmit.numberPerson < 1 || this.dataSubmit.numberPerson > 10) {
+                                alert('Please enter a minimum number of 1 person and a maximum of 10 people');
+                                return;
+                            }
+                            this.dish = this.filteredDishesMeal();
+                            console.log(this.dish)
                             break;
                         case 2:
+                            if (!this.dataSubmit.restaurant) {
+                                alert('Please Select a restaurant');
+                                return;
+                            }
+                            this.dish = this.filteredDishesMealRestaurant();
+                            console.log(this.dish)
 
                             break;
                         case 3:
+                            this.dish = this.filteredAll();
+                            let totalDishes = this.dataSubmit.dish.reduce((total, item) => total + item.number, 0);
 
+                            if (totalDishes < this.dataSubmit.numberPerson) {
+                                alert('Total number of dishes must be greater than or equal to the number of persons');
+                                return;
+                            }
+
+                            if (totalDishes > 10) {
+                                alert('Total number of dishes cannot exceed 10');
+                                return;
+                            }
+                            console.log(this.dish)
                             break;
 
                     }
@@ -199,6 +291,13 @@
                 pre() {
                     if (this.step > 1) {
                         this.step -= 1;
+                        if(this.step == 1){
+                            this.dish = this.filteredDishesMeal();
+                        }else if(this.step == 2){
+                            this.dish = this.filteredDishesMealRestaurant();
+                            this.dataSubmit.dish = [];
+
+                        }
                     }
                 }
             }
